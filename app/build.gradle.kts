5ad8223242a7031104f7f2b8ae9c0ba7030f64a6
Praveen Peiris <129660222+peirispraveen@users.noreply.github.com>
2024-03-06 01:35:21 +0530

    TFLite Integration test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("org.tensorflow:tensorflow-lite-task-vision-play-services:0.4.2")
+    implementation ("com.google.android.gms:play-services-tflite-gpu:16.2.0")
+    implementation ("org.tensorflow:tensorflow-lite:2.8.0")
 }
