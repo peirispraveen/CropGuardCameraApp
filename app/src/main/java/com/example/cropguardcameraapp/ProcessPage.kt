50e25dee28136d16c5775933de03d05391bd6588
Praveen Peiris <129660222+peirispraveen@users.noreply.github.com>
2024-03-08 11:16:14 +0530

    TFLite Integration 5

--- app/src/main/java/com/example/cropguardcameraapp/ProcessPage.kt
+++ app/src/main/java/com/example/cropguardcameraapp/ProcessPage.kt
@@ -20,7 +20,8 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
-import com.example.cropguardcameraapp.ml.PotatoMobilenetTflite
+import com.example.cropguardcameraapp.ml.PotatoMobilenetv2
+import com.example.cropguardcameraapp.ml.CropTflite
 
 
 class ProcessPage : AppCompatActivity() {
@@ -49,13 +50,18 @@ class ProcessPage : AppCompatActivity() {
         imageView = findViewById(R.id.imageView)
 
 
-        camera.setOnClickListener { view ->
-            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                startActivityForResult(cameraIntent, 3)
-            } else {
-                requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
-            }
+//        camera.setOnClickListener { view ->
+//            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+//                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+//                startActivityForResult(cameraIntent, 3)
+//            } else {
+//                requestPermissions(arrayOf(Manifest.permission.CAMERA), 100)
+//            }
+//        }
+
+        camera.setOnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent, 3)
         }
 
         gallery.setOnClickListener { view ->
@@ -99,8 +105,51 @@ class ProcessPage : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
     }
 
+    private fun classifyCrop(image: Bitmap) {
+        val cropModel = CropTflite.newInstance(applicationContext)
+
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 256, 256, 3), DataType.FLOAT32)
+        val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
+        byteBuffer.order(ByteOrder.nativeOrder())
+
+        val intValues = IntArray(imageSize * imageSize)
+        image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
+        var pixel = 0
+        for (i in 0 until imageSize) {
+            for (j in 0 until imageSize) {
+                val value = intValues[pixel++]
+                byteBuffer.putFloat(((value shr 16) and 0xFF) * (1f / 255))
+                byteBuffer.putFloat(((value shr 8) and 0xFF) * (1f / 255))
+                byteBuffer.putFloat((value and 0xFF) * (1f / 255))
+            }
+        }
+
+        inputFeature0.loadBuffer(byteBuffer)
+
+        val outputs = cropModel.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        val confidences = outputFeature0.floatArray
+
+        var maxPos = 0
+        var maxConfidence = 0f
+        for (i in confidences.indices) {
+            if (confidences[i] > maxConfidence) {
+                maxConfidence = confidences[i]
+                maxPos = i
+            }
+        }
+        val classes = arrayOf("paddy", "potato", "tea")
+        result.text = classes[maxPos]
+
+        cropModel.close()
+
+    }
+
+
+
     private fun classifyImage(image: Bitmap) {
-        val model = PotatoMobilenetTflite.newInstance(applicationContext)
+        val model = PotatoMobilenetv2.newInstance(applicationContext)
 
         val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 256, 256, 3), DataType.FLOAT32)
         val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
@@ -142,158 +191,3 @@ class ProcessPage : AppCompatActivity() {
 
 
 }
-
-
-//package com.example.cropguardcameraapp
-//
-//import android.Manifest
-//import android.content.Intent
-//import android.content.pm.PackageManager
-//import android.graphics.Bitmap
-//import android.os.Bundle
-//import android.provider.MediaStore
-//import android.widget.Button
-//import android.widget.ImageView
-//import android.widget.TextView
-//import androidx.appcompat.app.AppCompatActivity
-//
-//
-//import org.tensorflow.lite.DataType;
-//import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
-//
-//import java.nio.ByteBuffer
-//import java.nio.ByteOrder
-//
-////import com.example.cropguardcameraapp.ml.PotatoMobilenetTflite
-//import com.example.cropguardcameraapp.ml.PotatoMobilenetv2
-//
-//class ProcessPage : AppCompatActivity() {
-//    lateinit var camera: Button
-//    lateinit var gallery: Button
-//    lateinit var imageView: ImageView
-//    lateinit var result: TextView
-//
-//    val imageSize = 256
-//
-//    private val CAMERA_PERMISSION_CODE = 100
-//    private val STORAGE_PERMISSION_CODE = 101
-//    private val IMAGE_PICK_REQUEST_CODE = 102
-//    private val CAMERA_REQUEST_CODE = 103
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.process_pic)
-//
-//        camera = findViewById(R.id.button)
-//        gallery = findViewById(R.id.button2)
-//        result = findViewById(R.id.result)
-//        imageView = findViewById(R.id.imageView)
-//
-//        camera.setOnClickListener {
-//            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-//                openCamera()
-//            } else {
-//                requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
-//            }
-//        }
-//
-//        gallery.setOnClickListener {
-//            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-//                openGallery()
-//            } else {
-//                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
-//            }
-//        }
-//    }
-//
-//    private fun openCamera() {
-//        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
-//    }
-//
-//    private fun openGallery() {
-//        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-//        galleryIntent.type = "image/*"
-//        startActivityForResult(galleryIntent, IMAGE_PICK_REQUEST_CODE)
-//    }
-//
-//    @Deprecated("Deprecated in Java")
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        if (resultCode == RESULT_OK) {
-//            when (requestCode) {
-//                CAMERA_REQUEST_CODE -> {
-//                    val image = data?.extras?.get("data") as? Bitmap ?: return
-//                    displayImage(image)
-//                    classifyImage(image)
-//                }
-//                IMAGE_PICK_REQUEST_CODE -> {
-//                    val selectedImageUri = data?.data ?: return
-//                    val imageBitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImageUri)
-//                    displayImage(imageBitmap)
-//                    classifyImage(imageBitmap)
-//                }
-//            }
-//        }
-//    }
-//
-//    private fun displayImage(image: Bitmap) {
-//        imageView.setImageBitmap(image)
-//    }
-//
-//    private fun classifyImage(image: Bitmap) {
-//        val model = PotatoMobilenetv2.newInstance(applicationContext)
-//
-//        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 256, 256, 3), DataType.FLOAT32)
-//        val byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3)
-//        byteBuffer.order(ByteOrder.nativeOrder())
-//
-//        val intValues = IntArray(imageSize * imageSize)
-//        image.getPixels(intValues, 0, image.width, 0, 0, image.width, image.height)
-//        var pixel = 0
-//        for (i in 0 until imageSize) {
-//            for (j in 0 until imageSize) {
-//                val value = intValues[pixel++]
-//                byteBuffer.putFloat(((value shr 16) and 0xFF) * (1f / 255))
-//                byteBuffer.putFloat(((value shr 8) and 0xFF) * (1f / 255))
-//                byteBuffer.putFloat((value and 0xFF) * (1f / 255))
-//            }
-//        }
-//
-//        inputFeature0.loadBuffer(byteBuffer)
-//
-//        val outputs = model.process(inputFeature0)
-//        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-//
-//        val confidences = outputFeature0.floatArray
-//
-//        var maxPos = 0
-//        var maxConfidence = 0f
-//        for (i in confidences.indices) {
-//            if (confidences[i] > maxConfidence) {
-//                maxConfidence = confidences[i]
-//                maxPos = i
-//            }
-//        }
-//        val classes = arrayOf("Potato___Early_blight", "Potato___Late_blight", "Potato___healthy")
-//        result.text = classes[maxPos]
-//
-//        model.close()
-//    }
-//
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        when (requestCode) {
-//            CAMERA_PERMISSION_CODE -> {
-//                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    openCamera()
-//                }
-//            }
-//            STORAGE_PERMISSION_CODE -> {
-//                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    openGallery()
-//                }
-//            }
-//        }
-//    }
-//}
